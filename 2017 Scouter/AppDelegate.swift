864cc9f5b9105898a91b3d14a89220f8f7f4ada5
Ujjwal-N <ujjwal.nadhani@gmail.com>
2016-12-18 16:27:47 -0800

    finished menu screen

--- 2017 Scouter/AppDelegate.swift
+++ 2017 Scouter/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var ref: FIRDatabaseReference!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
         // Override point for customization after application launch.
         FIRApp.configure()
         FIRAuth.auth()?.signInAnonymously() { (user, error) in
--- 2017 Scouter/ViewControllers/TSMenuViewController.swift
+++ 2017 Scouter/ViewControllers/TSMenuViewController.swift
@@ -10,11 +10,41 @@ import Foundation
 import UIKit
 
 class MenuViewController: UIViewController {
-//    @IBOutlet weak var myButton : UIButton!
-//    @IBOutlet weak var myTextField : UITextField!
-//    
-//    @IBAction func myButtonAction(sender: id)
-    @IBAction func menuUnwind(unwindSegue: UIStoryboardSegue) {
+    
+    
+    @IBOutlet var menuButtons: [UIButton]!
+    
+    
+    override func viewDidLoad() {
+
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //Gradient
+        let gradient:CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.view.frame
+        var color1 = UIColor(colorLiteralRed: 34/255, green: 139/255, blue: 34/255, alpha: 1).cgColor
+        var color2 = UIColor(colorLiteralRed: 17/255, green: 38/255, blue: 11/255, alpha: 1).cgColor
+        gradient.colors = [color1, color2] //Or any colors
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+        //Borders
+        for menuButton in menuButtons{
+            //Borders
+            menuButton.layer.borderWidth = 1
+            menuButton.layer.borderColor = UIColor.white.cgColor
+            
+        }
+        
+
+    }
+    
+    
+    //White status bar
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent;
+    }
+
 }
