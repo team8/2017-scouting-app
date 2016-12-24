8225faae7ff0d6dbd9aed2cf370172f9f8ccd075
Ujjwal-N <ujjwal.nadhani@gmail.com>
2016-12-24 12:31:41 -0800

    Added changes to storyboard, not done

---
+++ 2017 Scouter/UnplayedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UnplayedTableViewCell.swift
+//  2017 Scouter
+//
+//  Created by Ujjwal Nadhani on 12/23/16.
+//  Copyright © 2016 Team 8. All rights reserved.
+//
+
+import UIKit
+
+class UnplayedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
