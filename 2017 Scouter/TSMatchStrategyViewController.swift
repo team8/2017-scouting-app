0f5ce6783277039a9a47482bda3552469147a59a
Robbie Selwyn <robbie@selwynsoftware.com>
2017-03-29 20:41:54 -0700

    Finished up strategy page

--- 2017 Scouter/TSMatchStrategyViewController.swift
+++ 2017 Scouter/TSMatchStrategyViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class MatchStrategyViewController : UIViewController {
+class MatchStrategyViewController : ViewController {
     
     static var match : TBAMatch?
     
@@ -23,10 +23,57 @@ class MatchStrategyViewController : UIViewController {
         titleText.text = MatchStrategyViewController.match?.key.replacingOccurrences(of: "2017", with: "")
         red.isScrollEnabled = true
         blue.isScrollEnabled = true
+        red.isEditable = false
+        blue.isEditable = false
         
-        red.text = ""
         blue.text = ""
+        red.text = ""
         
+        for team in (MatchStrategyViewController.match?.blue)! {
+            blue.text = blue.text + "Team #\(team.teamNumber)\n\n"
+            blue.text = blue.text + getData(team: team)
+        }
+
+        for team in (MatchStrategyViewController.match?.red)! {
+            red.text = red.text + "Team #\(team.teamNumber)\n\n"
+            red.text = red.text + getData(team: team)
+        }
+    }
+    
+    func getData(team : Team) -> String{
+        var retVal : String = ""
+        
+        // Begin Climb
+        
+        let percent : Double = team.importantStats["End-Takeoff-Achieve-Rate"]! as! Double
+        print(percent)
+        retVal = "Climb: \((round(1000*percent)/10))%"
+        
+        var climbCount : Int = 0
+        var total : Int = 0
+        
+        for match in team.matches {
+            let timd = Data.getTIMD(team: team, match: match)
+            if (timd == nil) {
+                continue
+            }
+            
+            if timd?.stats["End-Takeoff"] as! String == "2" {
+                climbCount += 1
+            }
+            total += 1
+        }
+        
+        retVal += "(\(climbCount) of \(total)) \n"
+        // End Climb
+        
+        retVal += "Climb Speed: \(team.importantStats["End-Takeoff-Speed-Average"]!)\n"
+        
+        
+        let cycle : Double = team.importantStats["Tele-Gears-Cycles-Average"]! as! Double
+        retVal += "Gear Count Average: \(round(1000*cycle)/1000) \n\n"
+    
+        return retVal
     }
     
 }
